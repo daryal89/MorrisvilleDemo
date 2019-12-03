2aa60b62e5037db20283ebf8e0fde4869e0af9b9
daryal89 <creativity80@gmail.com>
2019-12-03 18:32:07 -0500

    package added

---
+++ src/main/java/developrepo1/Haripala.java
@@ -0,0 +1,10 @@
+package developrepo1;
+
+public class Haripala {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/developrepo1/Hellojava.java
@@ -0,0 +1,10 @@
+package developrepo1;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
