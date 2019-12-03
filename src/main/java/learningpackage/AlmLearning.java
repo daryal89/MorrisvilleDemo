444bee4047a699b4839e8f8d8a27b676dc5b9665
daryal89 <creativity80@gmail.com>
2019-12-03 17:24:19 -0500

    Usa latest commit

---
+++ src/main/java/learningpackage/AlmLearning.java
@@ -0,0 +1,10 @@
+package learningpackage;
+
+public class AlmLearning {
+
+	public static void main(String[] args) {
+		System.out.println("jiraLearning!!1");
+
+	}
+
+}
---
+++ src/main/java/learningpackage/LearnJira.java
@@ -0,0 +1,10 @@
+package learningpackage;
+
+public class LearnJira {
+
+	public static void main(String[] args) {
+		System.out.println("jiraLearning!!1");
+
+	}
+
+}
