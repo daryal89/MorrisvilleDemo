3827b134819bb7f84525e8363ba7502742d2d1b9
daryal89 <creativity80@gmail.com>
2019-12-03 17:18:04 -0500

    Asian guy commit

---
+++ src/main/java/demopackage/Postman.java
@@ -0,0 +1,8 @@
+package demopackage;
+
+public class Postman {
+
+	public static void main(String[] args) {
+		System.out.println("Appium test!!!");
+	}
+}
---
+++ src/main/java/demopackage/RestAPI.java
@@ -0,0 +1,8 @@
+package demopackage;
+
+public class RestAPI {
+
+	public static void main(String[] args) {
+		System.out.println("Appium test!!!");
+	}
+}
